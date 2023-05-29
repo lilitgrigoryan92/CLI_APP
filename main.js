311730f73354d3ed436b7ec864d7d080598133cd
lilitgrigoryan92 <lilittgrigoryan@gmail.com>
2023-05-29 02:32:55 -0700

    use switch statement

--- main.js
+++ main.js
@@ -1,50 +1,64 @@
-import os from "node:os"
+import os from "os"
+
 
 const username = os.userInfo().username;
 
 console.log(`Welcome ${username}!`);
-process.stdout.write('> ');
-
-process.stdin.on('data', (input) => {
-  const command = input.toString().trim();
-
-  if (command === '.exit') {
-    console.log(`Thank you ${username}, goodbye!`);
-    process.exit();
-  } else if (command.startsWith('os')) {
-    const option = command.split(' ')[1];
-
-    if (option === '--cpus') {
-      const cpus = os.cpus();
-      console.log(`CPUs: ${cpus.length}`);
-      cpus.forEach((cpu, index) => {
-        console.log(`CPU ${index }: Model - ${cpu.model}, Clock Rate - ${cpu.speed} MHz`);
-      });
-    } else if (option === '--homedir') {
-      console.log(`Home Directory: ${os.homedir()}`);
-    } else if (option === '--username') {
-      console.log(`Username: ${os.userInfo().username}`);
-    } else if (option === '--architecture') {
-      console.log(`CPU Architecture: ${os.arch()}`);
-    } else if (option === '--hostname') {
-      console.log(`Hostname: ${os.hostname()}`);
-    } else if (option === '--platform') {
-      console.log(`Platform: ${os.platform()}`);
-    } else if (option === '--memory') {
-      const totalMemory = os.totalmem();
-      console.log(`Total memory: ${totalMemory} bytes`)
-
-    } else {
-      console.log('Invalid operation');
-    }
-  } else {
-    console.log('Invalid input');
-  }
-
-  process.stdout.write('> ');
-});
 
 process.on('SIGINT', () => {
   console.log(`Thank you ${username}, goodbye!`);
   process.exit();
+});
+
+function call(command) {
+  const [operation, argument] = command.split(' ');
+
+  switch (operation) {
+    case '.exit':
+      process.emit('SIGINT');
+      break;
+    case 'os':
+        callCommandOs(argument);
+      break;
+    default:
+      console.log('Invalid input. Please enter a valid command.');
+  }
+}
+
+function callCommandOs(argument) {
+  switch (argument) {
+    case '--cpus':
+      const cpus = os.cpus();
+      cpus.forEach((cpu, index) => {
+        console.log(`CPU ${index + 1}:`);
+        console.log(`  Model: ${cpu.model}`);
+        console.log(`  Clock Rate: ${cpu.speed / 1000} GHz`);
+      });
+      break;
+    case '--homedir':
+      console.log(`Home Directory: ${os.homedir()}`);
+      break;
+    case '--username':
+      console.log(`Username: ${username}`);
+      break;
+    case '--architecture':
+      console.log(`CPU Architecture: ${os.arch()}`);
+      break;
+    case '--hostname':
+      console.log(`Hostname: ${os.hostname()}`);
+      break;
+    case '--platform':
+      console.log(`Platform: ${os.platform()}`);
+      break;
+    case '--memory':
+      console.log(`Total Memory: ${os.totalmem()} bytes`);
+      break;
+    default:
+      console.log('Invalid input. Please enter a valid command.');
+  }
+}
+
+process.stdin.on('data', (data) => {
+  const command = data.toString().trim();
+  call(command);
 });
